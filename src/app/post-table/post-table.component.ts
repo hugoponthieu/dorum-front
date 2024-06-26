8d0a64c08ef1c866445745f5fe937fd0b534d80c
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-06-26 15:34:51 +0200

    feat(table):format table

--- src/app/post-table/post-table.component.ts
+++ src/app/post-table/post-table.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   templateUrl: './post-table.component.html',
 })
 export class PostTableComponent {
-
+  posts = [
+    { id: 1, name: 'Introduction to Angular', numberOfPosts: 10, lastPostDate: new Date('2024-01-15').toDateString() },
+    { id: 2, name: 'Advanced TypeScript', numberOfPosts: 5, lastPostDate: new Date('2024-02-20').toDateString() },
+    { id: 3, name: 'RxJS in Depth', numberOfPosts: 8, lastPostDate: new Date('2024-03-05').toDateString() }
+  ];
 }
