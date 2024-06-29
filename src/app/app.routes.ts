d81e1f06b9b7d83cc8c22300350d1be7153c24b5
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-06-29 12:51:52 +0200

    edit message

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeScreenComponent } from './home-screen/home-screen.component';
 import { TopicScreenComponent } from './topic-screen/topic-screen.component';
 import { PostsScreenComponent } from './posts-screen/posts-screen.component';
+import { EditMessageComponent } from './edit-message/edit-message.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeScreenComponent },
     {
-        path: 'topic', component: TopicScreenComponent, children: [
-            { path: ':id', component: PostsScreenComponent }
-
+        path: 'topic/:id', component: TopicScreenComponent, children: [
+            { path: '', component: PostsScreenComponent },
+            { path: 'message/:messageId', component: EditMessageComponent }
         ]
     }
 ];
--- src/app/edit-message/edit-message.component.ts
+++ src/app/edit-message/edit-message.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-message',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './edit-message.component.html',
 })
 export class EditMessageComponent {
-
+    content= new FormControl('')
+  onSubmit(){
+    console.log(this.content.value)
+  }
 }
