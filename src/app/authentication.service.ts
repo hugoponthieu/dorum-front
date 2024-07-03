5a4251441a1317e063a97a2d9270f6c1e41d92dc
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-07-03 12:07:09 +0200

    adding user storage

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -1,39 +1,59 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthenticationService {
+export class AuthService {
   private baseUrl = 'http://localhost:3333/auth';
+  private currentUserSubject: BehaviorSubject<any>;
+  public currentUser: Observable<any>;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    const user = JSON.parse(localStorage.getItem('current_user') ?? '{}');
+    this.currentUserSubject = new BehaviorSubject<any>(user);
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): any {
+    return this.currentUserSubject.value;
+  }
 
   signin(credentials: { email: string, password: string }): Observable<any> {
     return this.http.post<any>(`${this.baseUrl}/signin`, credentials).pipe(
-      tap(response => this.storeToken(response.token))
+      tap(response => this.handleAuthentication(response))
     );
   }
 
   signup(data: { name: string, email: string, password: string }): Observable<any> {
-    return this.http.post<any>(`${this.baseUrl}/signup`, data)
+    return this.http.post<any>(`${this.baseUrl}/signup`, data).pipe(
+      tap(response => this.handleAuthentication(response))
+    );
   }
 
-  private storeToken(token: string): void {
-    localStorage.setItem('auth_token', token);
-  }
-
-  getToken(): string | null {
-    return localStorage.getItem('auth_token');
-  }
-
-  isAuthenticated(): boolean {
-    return !!this.getToken();
+  private handleAuthentication(response: any): void {
+    localStorage.setItem('auth_token', response.token);
+    localStorage.setItem('current_user', JSON.stringify(response.user));
+    this.currentUserSubject.next(response.user);
   }
 
   logout(): void {
     localStorage.removeItem('auth_token');
+    localStorage.removeItem('current_user');
+    this.currentUserSubject.next(null);
+  }
+
+  isAuthenticated(): boolean {
+    return !!localStorage.getItem('auth_token');
+  }
+
+  getUserRights(): any {
+    return this.currentUserValue?.rights || [];
+  }
+
+  hasRight(right: string): boolean {
+    return this.getUserRights().includes(right);
   }
 }
