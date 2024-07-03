6d091ee6d25e1ff305188c6fd43d8df7529205dc
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-07-03 11:00:57 +0200

    auth login

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Post } from './models/post';
 
@@ -8,26 +8,33 @@ import { Post } from './models/post';
 })
 export class PostsService {
   private baseUrl = 'http://localhost:3333/topics';
+  private token = 'oat_Ng.MFpaS1dHaFg2bzlrVkpUeHpYYVZ0S3BzbFZfZW1TM3BnMEp6bVlGXzM4ODAzOTYyODA';
 
   constructor(private http: HttpClient) { }
 
+  private getHeaders() {
+    return new HttpHeaders({
+      'Authorization': `Bearer ${this.token}`
+    });
+  }
+
   getPostsByTopic(topicId: string): Observable<Post[]> {
-    return this.http.get<Post[]>(`${this.baseUrl}/${topicId}/posts`);
+    return this.http.get<Post[]>(`${this.baseUrl}/${topicId}/posts`, { headers: this.getHeaders() });
   }
 
   getPostById(topicId: string, postId: string): Observable<Post> {
-    return this.http.get<Post>(`${this.baseUrl}/${topicId}/posts/${postId}`);
+    return this.http.get<Post>(`${this.baseUrl}/${topicId}/posts/${postId}`, { headers: this.getHeaders() });
   }
 
   createPost(topicId: string, post: Partial<Post>): Observable<Post> {
-    return this.http.post<Post>(`${this.baseUrl}/${topicId}/posts`, post);
+    return this.http.post<Post>(`${this.baseUrl}/${topicId}/posts`, post, { headers: this.getHeaders() });
   }
 
   updatePost(topicId: string, postId: string, post: Partial<Post>): Observable<Post> {
-    return this.http.put<Post>(`${this.baseUrl}/${topicId}/posts/${postId}`, post);
+    return this.http.put<Post>(`${this.baseUrl}/${topicId}/posts/${postId}`, post, { headers: this.getHeaders() });
   }
 
   deletePost(topicId: string, postId: string): Observable<void> {
-    return this.http.delete<void>(`${this.baseUrl}/${topicId}/posts/${postId}`);
+    return this.http.delete<void>(`${this.baseUrl}/${topicId}/posts/${postId}`, { headers: this.getHeaders() });
   }
 }
--- src/app/topics.service.ts
+++ src/app/topics.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Topic } from './models/topic';
 
@@ -8,26 +8,33 @@ import { Topic } from './models/topic';
 })
 export class TopicsService {
   private baseUrl = 'http://localhost:3333/topics';
+  private token = 'oat_Ng.MFpaS1dHaFg2bzlrVkpUeHpYYVZ0S3BzbFZfZW1TM3BnMEp6bVlGXzM4ODAzOTYyODA';  // Replace with your actual token
 
   constructor(private http: HttpClient) { }
 
+  private getHeaders() {
+    return new HttpHeaders({
+      'Authorization': `Bearer ${this.token}`
+    });
+  }
+
   getAllTopics(): Observable<Topic[]> {
-    return this.http.get<Topic[]>(`${this.baseUrl}`);
+    return this.http.get<Topic[]>(`${this.baseUrl}`, { headers: this.getHeaders() });
   }
 
   getTopicById(topicId: string): Observable<Topic> {
-    return this.http.get<Topic>(`${this.baseUrl}/${topicId}`);
+    return this.http.get<Topic>(`${this.baseUrl}/${topicId}`, { headers: this.getHeaders() });
   }
 
   createTopic(topic: Partial<Topic>): Observable<Topic> {
-    return this.http.post<Topic>(`${this.baseUrl}`, topic);
+    return this.http.post<Topic>(`${this.baseUrl}`, topic, { headers: this.getHeaders() });
   }
 
   updateTopic(topicId: string, topic: Partial<Topic>): Observable<Topic> {
-    return this.http.put<Topic>(`${this.baseUrl}/${topicId}`, topic);
+    return this.http.put<Topic>(`${this.baseUrl}/${topicId}`, topic, { headers: this.getHeaders() });
   }
 
   deleteTopic(topicId: string): Observable<void> {
-    return this.http.delete<void>(`${this.baseUrl}/${topicId}`);
+    return this.http.delete<void>(`${this.baseUrl}/${topicId}`, { headers: this.getHeaders() });
   }
 }
